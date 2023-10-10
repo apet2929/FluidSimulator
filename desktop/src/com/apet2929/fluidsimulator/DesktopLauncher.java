07888d9a6b038481ef8b7852f2c457730a3a1749
apet2929 <realandrewpetersen@gmail.com>
2023-10-10 10:05:14 -0400

    Initial commit

---
+++ desktop/src/com/apet2929/fluidsimulator/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.apet2929.fluidsimulator;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.apet2929.fluidsimulator.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("FluidSimulator");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
